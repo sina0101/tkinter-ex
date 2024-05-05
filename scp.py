59d0e59a7f5476622eb416c0788a393446f21a01
Sina Jafari <jafarisina52@gmail.com>
2024-05-05 14:45:22 +0330

    initial commit

--- scp.py
+++ scp.py
@@ -1,23 +1,21 @@
 import json
-import tkinter
-from tkinter import *
+from customtkinter import *
 import tkinter.messagebox
 
-win1=Tk()
-win1.geometry('700x500')
-L1=Label(win1,text='hi it is first LB',fg='red',padx=100,pady=100)
+win1=CTk()
+#win1.geometry('700x500')
+L1=CTkLabel(win1,text='hi it is first LB')
 L1.pack()
 
-name=Label(win1,text='full name')
+name=CTkLabel(win1,text='full name')
 name.pack()
 
-e1=Entry(win1)
+e1=CTkEntry(win1)
 e1.pack()
-
-age=Label(win1,text='age')
+win1.geometry('300x300')
+age=CTkLabel(win1,text='age')
 age.pack()
-
-e2=Entry(win1)
+e2=CTkEntry(win1)
 e2.pack()
 dic1={}
 def def1():
@@ -26,15 +24,15 @@ def def1():
         json.dump(dic1,f)
         
         
-btn1=Button(win1,text='SUB',fg='red',state=NORMAL,command=def1)
+btn1=CTkButton(win1,text='SUB',state=NORMAL,command=def1,border_width=2,)
 btn1.pack()
 
 def def2():
     with open ('data.json','r') as f:
         data=f.readline()
     tkinter.messagebox.showinfo(win1,data)
-
-btn2=Button(win1,text='read',fg='blue',command=def2)
+    
+btn2=CTkButton(win1,text='read',command=def2,border_width=2)
 btn2.pack()
 
 win1.mainloop()
